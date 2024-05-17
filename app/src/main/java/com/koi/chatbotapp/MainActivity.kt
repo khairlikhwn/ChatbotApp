d3ee72d18ab2139f926d5c1cda4a034647934ef9
khairlikhwn <khairulmn83@gmail.com>
2024-05-17 08:51:47 +0800

    Initial commit

---
+++ app/src/main/java/com/koi/chatbotapp/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.koi.chatbotapp
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.koi.chatbotapp.screen.ChatRoomListScreen
+import com.koi.chatbotapp.screen.ChatScreen
+import com.koi.chatbotapp.screen.LoginScreen
+import com.koi.chatbotapp.screen.SignUpScreen
+import com.koi.chatbotapp.ui.theme.ChatbotAppTheme
+import com.koi.chatbotapp.viewmodel.AuthViewModel
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            val authViewModel: AuthViewModel = viewModel()
+            ChatbotAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavigationGraph(navController = navController, authViewModel = authViewModel)
+                }
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun NavigationGraph(
+    navController: NavHostController,
+    authViewModel: AuthViewModel
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.SignupScreen.route
+    ) {
+        composable(Screen.SignupScreen.route) {
+            SignUpScreen(
+                authViewModel = authViewModel,
+                onNavigateToLogin = { navController.navigate(Screen.LoginScreen.route) }
+            )
+        }
+        composable(Screen.LoginScreen.route) {
+            LoginScreen(
+                authViewModel = authViewModel,
+                onNavigateToSignUp = { navController.navigate(Screen.SignupScreen.route) }
+            ) {
+                navController.navigate(Screen.ChatRoomsScreen.route)
+            }
+        }
+        composable(Screen.ChatRoomsScreen.route) {
+            ChatRoomListScreen {
+                navController.navigate("${Screen.ChatScreen.route}/${it.id}")
+            }
+        }
+        composable("${Screen.ChatScreen.route}/{roomId}") {
+            val roomId: String = it
+                .arguments?.getString("roomId") ?: ""
+            ChatScreen(roomId = roomId)
+        }
+    }
+}
---
+++ app/src/main/java/com/koi/chatbotapp/data/Room.kt
@@ -0,0 +1,6 @@
+package com.koi.chatbotapp.data
+
+data class Room(
+    val id: String = "",
+    val name: String = ""
+)
